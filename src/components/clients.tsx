8d9b9162afc091af1df3abe381b5a4fd995d7a86
Julia Taro <julia.taro@gmail.com>
2025-09-26 20:44:51 +0300

    Clients 2 rows view

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 import Box from "@mui/system/Box";
-import Grid from "@mui/system/Grid"; // Grid v2 from @mui/system
-import "../index.css";
-
-// import imgBiospa from "../assets/logos/biospa.png";
-// import imgSpaVarska from "../assets/logos/varska_spa.png";
-// import imgAlmarPuit from "../assets/logos/almar_puit.png";
-// import imgLionsClub from "../assets/logos/lions_club.png";
-// import imgSilmaTark from "../assets/logos/silmatark_logo.png";
-// import imgKinnisvara from "../assets/logos/mp_kinnisvara.png";
-// import imgTobrox from "../assets/logos/tobrox_wood.png";
-// import imgTvPlay from "../assets/logos/tvplay.png";
-
-import imgBiospa from "../assets/logos_c/biospa.png";
+import Grid from "@mui/system/Grid";
+import styled from "@mui/system/styled";
+import CardMedia from "@mui/material/CardMedia";
+import imgBiospa from "../assets/logos/biospa.png";
 import imgSpaVarska from "../assets/logos/varska_spa.png";
 import imgAlmarPuit from "../assets/logos/almar_puit.png";
 import imgLionsClub from "../assets/logos/lions_club.png";
@@ -20,54 +11,107 @@ import imgSilmaTark from "../assets/logos/silmatark_logo.png";
 import imgKinnisvara from "../assets/logos/mp_kinnisvara.png";
 import imgTobrox from "../assets/logos/tobrox_wood.png";
 import imgTvPlay from "../assets/logos/tvplay.png";
-import { maxWidth, width } from "@mui/system";
 
-const logos = [
-  { src: imgBiospa, alt: "Biospa" },
-  { src: imgSpaVarska, alt: "Spa Värska", mt: -4 },
-  { src: imgAlmarPuit, alt: "Almar Puit", mt: -2 },
-  { src: imgLionsClub, alt: "Lions Club", mt: -2 },
-  { src: imgSilmaTark, alt: "Silma Tark", mt: -10 },
-  { src: imgKinnisvara, alt: "MP Kinnisvara", mt: -5 },
-  { src: imgTobrox, alt: "TobroxWood", mt: -3 },
-  { src: imgTvPlay, alt: "TV Play", mt: -10 },
-];
+import "../index.css";
 
-export default function ClientLogos() {
+const Item = styled("div")(({ theme }) => ({
+  backgroundColor: "#e7e7f1",
+  border: "1px solid",
+  borderColor: "#e7e7f1",
+  padding: theme.spacing(1),
+  borderRadius: "4px",
+  textAlign: "center",
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+    borderColor: "#444d58",
+  }),
+}));
+
+export default function FullWidthGrid() {
   return (
-    <Box sx={{ px: { xs: 1, sm: 2, md: 6 } }}>
-      <Grid
-        container
-        spacing={{ xs: 1.5, md: 3 }}
-        alignItems="center"
-        justifyContent="center"
-        wrap="nowrap" // single row
-        sx={{ maxWidth: "100%", mx: "auto" }}
-      >
-        {logos.map((l, idx) => (
-          <Grid
-            key={l.alt}
-            size={{ xs: "auto" }} // <-- Grid v2 uses `size`
-            sx={{
-              flex: "0 0 auto", // don't shrink
-              display: { xs: idx >= 4 ? "none" : "block", sm: "block" }, // hide last 4 on xs
-            }}
-          >
-            <Box
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 2 }}>
+          <Item sx={{ border: 0 }}></Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia component="img" image={imgBiospa} alt="Paella dish" />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-4px" }}
               component="img"
-              src={l.src}
-              alt={l.alt}
-              loading="lazy"
-              sx={{
-                display: "block",
-                height: 48, // constant visual size (tweak if needed)
-                width: 100,
-                objectFit: "contain",
-                mt: l.mt ? `${l.mt}px` : 0,
-              }}
+              image={imgSpaVarska}
+              alt="Spa Värska"
             />
-          </Grid>
-        ))}
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-19px" }}
+              component="img"
+              image={imgAlmarPuit}
+              alt="Almar Puit"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-14px" }}
+              component="img"
+              image={imgLionsClub}
+              alt="Lions Club"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-33px" }}
+              component="img"
+              image={imgSilmaTark}
+              alt="Silma Tark"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-14px" }}
+              component="img"
+              image={imgKinnisvara}
+              alt="MP Kinnisvara"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-3px" }}
+              component="img"
+              image={imgTobrox}
+              alt="TobroxWood"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 3, md: 1 }}>
+          <Item sx={{ border: 0 }}>
+            <CardMedia
+              sx={{ marginTop: "-20px" }}
+              component="img"
+              image={imgTvPlay}
+              alt="TV Play"
+            />
+          </Item>
+        </Grid>
+        <Grid size={{ xs: 12, md: 2 }}>
+          <Item sx={{ border: 0 }}></Item>
+        </Grid>
       </Grid>
     </Box>
   );
