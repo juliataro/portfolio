90ed707a7a46e4bb086d37f7e070ab3f157e4cf2
Julia Taro <julia.taro@gmail.com>
2026-01-28 15:49:43 +0200

    Logo, Title, and Web Page Name changing

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -76,7 +76,7 @@ export default function Navigation() {
                   height: 30,
                   pt: 0.5,
                   mr: 1,
-                  mt: 0.2,
+                  mt: -1,
                   objectFit: "contain",
                 }}
               />
@@ -84,7 +84,7 @@ export default function Navigation() {
           </Box>
 
           {/* Title */}
-          <Box sx={{ flexGrow: 1, mt: 1.2 }}>
+          <Box sx={{ flexGrow: 1, mt: -2 }}>
             <Typography variant="h6">Web Development</Typography>
           </Box>
 
